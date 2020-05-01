ad4a223f11e0e29546b4948c516b8953f29d42be
Lucas Lima <luc.tech3337@gmail.com>
2020-05-01 00:31:56 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', carregar)
+function carregar(){
+    var img = window.document.getElementById('img1')
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes()
+    var div1 = window.document.getElementById('div1')
+    if(min <= 9 && min >= 0){
+        div1.innerHTML = `Agora são exatamente ${hora}:0${min}`
+    }
+    else{
+        div1.innerHTML = `Agora são exatamente ${hora}:${min}`
+    }
+    if(hora >= 12 && hora <= 18){
+        img.src = 'imagens/tarde.jpg'
+        window.document.body.style.backgroundColor = 'rgb(27, 54, 54)'
+        div1.style.backgroundColor = 'rgb(27, 54, 54)'
+    }
+    else if(hora > 18 || hora <= 4){
+        img.src = 'imagens/noite.jpg'
+        window.document.body.style.backgroundColor = 'rgb(41, 34, 34)'
+        div1.style.backgroundColor = 'rgb(41, 34, 34)'
+    }
+}
